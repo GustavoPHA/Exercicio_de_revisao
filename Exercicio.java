9d17962572e0dbc7ab6295b1658e73fb792e931e
Gustavo Philip <111594616+GustavoPHA@users.noreply.github.com>
2022-08-23 20:09:45 -0300

    Update Exercicio.java
    Alteração

--- Exercicio.java
+++ Exercicio.java
@@ -1,18 +1,25 @@
 import java.util.Scanner;
+public class Exercicio {
+    public static void main(String[]args){
+        Scanner ler = new Scanner(System.in);
 
-   public class Exercicio {
-     public static void main ( String[] args ) {
+        double somNegativos=0, somPositivos=0, i=0, num;
 
-          Scanner ler = new Scanner (System.in) ;
+        while (i<=9) {
+            System.out.println("Digite um número: ");
+            num = ler.nextDouble();
+            if (num == 0){
+                System.out.println("Este é um número nulo, Digite ouro número :");
+                num = ler.nextDouble();
+            }else if (num < 0){
+                somNegativos += num;
+            }else if (num > 0){
+                somPositivos += num;
+            } 
+            i +=1;   
+        }
+        ler.close();
+        System.out.println("A soma total dos números negativos é "+somNegativos+" e dos números positivos "+somPositivos+".");
 
-          double base, altura, area;
-          System.out.println("Insira o valor da base:");
-          base= ler.nextDouble();
-
-          System.out.println("Insira o valor da Altura:");
-          altura= ler.nextDouble();
-
-		  area = (base * altura)/2;
-          System.out.println("O valor da area é:"+ area);
-     }
-   }
+    }
+}
