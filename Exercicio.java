d952c93f9a1470734f2fe37e619ab8c6adb0e5e3
Gustavo Philip Azevedo <12114910@aluno.newtonpaiva.br>
2022-08-23 19:42:47 -0300

    criei um programa

--- Exercicio.java
+++ Exercicio.java
@@ -1,1 +1,18 @@
-dhfsdyfioudf
+import java.util.Scanner;
+
+   public class Exercicio {
+     public static void main ( String[] args ) {
+
+          Scanner ler = new Scanner (System.in) ;
+
+          double base, altura, area;
+          System.out.println("Insira o valor da base:");
+          base= ler.nextDouble();
+
+          System.out.println("Insira o valor da Altura:");
+          altura= ler.nextDouble();
+
+		  area = (base * altura)/2;
+          System.out.println("O valor da area é:"+ area);
+     }
+   }
